11a8dd14364e5d8e7c0b21c71909eed124f52366
Максим Ткачев <maximtkachev99@gmail.com>
2020-06-04 17:24:37 +0400

    init

---
+++ app/js/common.js
@@ -0,0 +1,38 @@
+$(function() {
+
+$('input[type=tel]').mask('+7 (999) 999-99-99');
+
+$('.fancybox').fancybox();
+
+$('.js-modal-open').click(function(e) {
+	e.preventDefault();
+	$('body').addClass('overflow-hidden');
+	$( $(this).attr('href') ).fadeIn();
+});
+
+$('.modal__close').click(function() {
+	$('body').removeClass('overflow-hidden');
+	$(this).parents('.modal-wrapper').fadeOut();
+});
+
+$('.modal-wrapper').click(function(e) {
+	let modal = $(this).find('.modal');
+	if (modal.is(e.target) || modal.has(e.target).length > 0) return;
+	$('body').removeClass('overflow-hidden');
+	$(this).fadeOut();
+});
+
+$('.tabs__content').hide();
+$('.tabs__content:first').show();
+$('.tabs__list li:first').addClass('active');
+$('.tabs__list li').click(function(event) {
+	let parent = $(this).parents('.tabs');
+	parent.find('.tabs__list li').removeClass('active');
+	$(this).addClass('active');
+	parent.find('.tabs__content').hide();
+	var selectTab = $(this).find('a').attr("href");
+	$(selectTab).fadeIn();
+});
+
+
+});
